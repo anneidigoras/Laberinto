48b5ce8c0773f948e00b62edac4dfedb5b58bee2
Anne <anneidigoras@opendeusto.es>
2019-03-12 16:30:19 +0100

    Primera subida con algo avanzado

---
+++ R_templates/[TEMPLATE].problem-interface-XX.R
@@ -0,0 +1,97 @@
+# =======================================================================
+# Names:
+# Group Number: 
+# Assignment:
+# Date:
+# =======================================================================
+# 1. Be sure to include, with this template, any necessary files
+#    for execution, including datasets (problem.R, methodXXX.R, ...)
+#    (submission of the entire folder is recommended)
+# 2. If you use a function of a certain package, do not forget to include the
+#    corresponding call to the "library ()" function
+# 3. Do not forget to comment on the code, especially those non-trivial commands
+#    (remember that part of the rating depends on the cleaning of the code)
+# 4. It is strongly recommended to test any implemented function in order to 
+#    check for its proper operation
+# =======================================================================
+# (This is a general code, you must adapt it)
+# =======================================================================
+
+# =======================================================================
+# This function must return a list with the information needed to 
+# solve the problem.
+# (Depending on the problem, it should receive or not parameters)
+
+rows = 7
+columns = 7
+set.seed(123) #para aleatoriedad
+
+#0=pie izquierdo
+#1=pie derecho
+
+perm = sample(0:(rows*columns-1)) #nose cmo hacer para que la permutacion sea de ceros y unos
+state.initial = matrix(sample(0:1, rows*columns,replace=TRUE),rows,columns)
+#state.final   = matrix(sample(0:1,rows*columns,replace=TRUE),rows,columns)
+actions.possible = data.frame(action=c("Arriba","Abajo","Izquierda","Derecha"),cost=1)
+
+
+initialize.problem = function(rows=7, columns=7, perm = sample(0:(rows*columns-1))){
+  problem = list()
+  
+   problem$state.initial = matrix(perm,nrow=rows,byrow = TRUE)
+   problem$state.final   = matrix(0:(rows*columns-1),nrow=rows,byrow = TRUE)
+   problem$actions.possible = data.frame(action=c("Arriba","Abajo","Izquierda","Derecha"),cost=1)
+   problem$rows = rows
+   problem$columns = columns
+   problem$name = paste0("Problema de los zapatos (",rows,"x",columns,") - [",paste0(perm,collapse="-"),"]")
+
+  return(problem)
+}
+
+
+# =======================================================================
+# Must return TRUE or FALSE according with if the action can be done or not
+# over the specific state
+is.applicable = function (state, action, problem){
+  ...
+  return(...)
+}
+
+# =======================================================================
+# Must return the state resulting on applying the action over the state
+effect = function (state, action){
+  result = ...
+  return(...)
+}
+
+
+# =======================================================================
+# Must return TRUE or FALSE according with the state is final or not
+# * In case the final state is stablished by a condition, second argument
+#   could be omited
+is.final.state = function (state, finalstate){
+  ...
+  return(...)
+}
+
+# =======================================================================
+# Must print the state in console (in a legible way)
+to.string = function (state){
+  ...
+  print(...)
+}
+
+# =======================================================================
+# Return the cost of applying an action over a state
+get.cost = function (action,state){
+  ...
+  return(...)
+}
+
+# =======================================================================
+# (Used for Informed Algorithms)
+# Heuristic function used in Informed algorithms
+get.evaluation = function(state,problem){
+  ...
+  return(...)
+}
